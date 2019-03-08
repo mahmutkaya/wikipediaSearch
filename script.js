308481ee92b4088be1edac3e0769b5540d6a5739
mahmutKaya <33642821+mahmutkaya@users.noreply.github.com>
2019-03-08 04:16:41 +0100

    cleaning

--- script.js
+++ script.js
@@ -1,9 +1,58 @@
 "use strict";
 
 const mainPath = "https://en.wikipedia.org/wiki/";
-const randomPath = "https://en.wikipedia.org/wiki/Special:Random";
+const randomPath = mainPath + "Special:Random";
 const searchUrl =
   "https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*& srsearch=";
+
+const randomSearch = () =>
+  `<a href='${randomPath}' target='_blank'>I'm feeling lucky</a>`;
+
+async function fetchResults(userInput) {
+ try {
+   const result = await fetch(searchUrl + userInput);
+   const data = await result.json();
+   const searchQuery = data.query.search;
+   const totalHits = data.query.searchinfo.totalhits;
+   console.log(data.query);
+   totalHits === 0 ? errMsg(userInput) : renderData(searchQuery);
+ } catch (error) {
+   console.log(error);
+ }
+}
+
+const errMsg = keyword => {
+ let htmlString = `<div>
+ <p>No results found for <b><i>${keyword}</i></b> in wikipedia archive</p> 
+ </div>
+ `;
+ resultsDiv.innerHTML = "";
+ resultsDiv.insertAdjacentHTML("beforeEnd", htmlString);
+}
+
+const renderData = data => {
+ let htmlString = "";
+ for (const item of data) {
+   const url = encodeURI(mainPath + item.title);
+   htmlString += `
+       <div><h4>${item.title}</h4>${
+     item.snippet
+   }... <br>${item.timestamp.slice(0, 7)}<br>
+       <a href="${url}" target="_blank">${url}</a><hr></div>
+       `;
+ }
+ resultsDiv.innerHTML = "";
+ resultsDiv.insertAdjacentHTML("beforeEnd", htmlString);
+}
+
+const createAndAppend = (tag, append, innerHTML, id, Class) => {
+ const child = document.createElement(tag);
+ append.appendChild(child);
+ innerHTML !== undefined ? (child.innerHTML = innerHTML) : null;
+ id !== undefined ? (child.id = id) : null;
+ Class !== undefined ? (child.className = Class) : null;
+ return child;
+}
 
 const mainDiv = createAndAppend("div", document.body, null, "mainDiv");
 const logoDiv = createAndAppend("div", mainDiv, null, "logoDiv");
@@ -25,52 +74,3 @@ searchInputButton.addEventListener("click", () => {
     : fetchResults(searchInput.value);
 });
 
-function randomSearch() {
-  return `<a href='${randomPath}' target='_blank'>I'm feeling lucky</a>`;
-}
-
-async function fetchResults(userInput) {
-  try {
-    const result = await fetch(searchUrl + userInput);
-    const data = await result.json();
-    const searchQuery = data.query.search;
-    const totalHits = data.query.searchinfo.totalhits;
-    console.log(data.query);
-    totalHits === 0 ? errMsg(userInput) : renderData(searchQuery);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-function errMsg(keyword) {
-  let htmlString = `<div>
-  <p>No results found for <b><i>${keyword}</i></b> in wikipedia archive</p> 
-  </div>
-  `;
-  resultsDiv.innerHTML = "";
-  resultsDiv.insertAdjacentHTML("beforeEnd", htmlString);
-}
-
-function renderData(data) {
-  let htmlString = "";
-  for (const item of data) {
-    const url = encodeURI(mainPath + item.title);
-    htmlString += `
-        <div><h4>${item.title}</h4>${
-      item.snippet
-    }... <br>${item.timestamp.slice(0, 7)}<br>
-        <a href="${url}" target="_blank">${url}</a><hr></div>
-        `;
-  }
-  resultsDiv.innerHTML = "";
-  resultsDiv.insertAdjacentHTML("beforeEnd", htmlString);
-}
-
-function createAndAppend(tag, append, innerHTML, id, Class) {
-  const child = document.createElement(tag);
-  append.appendChild(child);
-  innerHTML !== undefined ? (child.innerHTML = innerHTML) : null;
-  id !== undefined ? (child.id = id) : null;
-  Class !== undefined ? (child.className = Class) : null;
-  return child;
-}
